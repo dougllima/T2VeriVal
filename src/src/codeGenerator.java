fae62011a719d2c9834a3c203eaeca66bd68fdb9
Douglas Lima <doug.llima@hotmail.com>
2017-05-23 16:00:21 -0300

    Adicionando biblioteca de geração de código.

---
+++ src/src/codeGenerator.java
@@ -0,0 +1,9 @@
+package src;
+
+/**
+ * Created by douglas.leite on 23/05/2017.
+ *
+ * Classe para gerar o código executavel atravez da biblioteca JavaPoet
+ */
+public class codeGenerator {
+}
---
+++ src/src/xmlParser.java
@@ -0,0 +1,10 @@
+package src;
+
+/**
+ * Created by douglas.leite on 23/05/2017.
+ *
+ * Classe para pegar o xml da maquina de Miley e transformar em uma sequencia de comandos.
+ */
+public class xmlParser {
+
+}
